dbbe10514a053d09b745721beb9bea054b53d128
Chris Kaczor <chris@kaczorzoo.net>
2014-07-19 08:29:07 -0400

    Add ability to relaunch as admin

---
+++ Security.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace Common
+{
+    public class Security
+    {
+        public static void EnsureElevatedInstall()
+        {
+            using (var wi = WindowsIdentity.GetCurrent())
+            {
+                if (wi == null)
+                    return;
+
+                var wp = new WindowsPrincipal(wi);
+
+                if (wp.IsInRole(WindowsBuiltInRole.Administrator))
+                    return;
+
+                using (var currentProc = Process.GetCurrentProcess())
+                using (var proc = new Process())
+                {
+                    proc.StartInfo.Verb = "runas";
+                    proc.StartInfo.FileName = currentProc.MainModule.FileName;
+                    proc.StartInfo.Arguments = "/reinstall";
+                    proc.Start();
+                }
+
+                Environment.Exit(0);
+            }
+        }
+    }
+}
