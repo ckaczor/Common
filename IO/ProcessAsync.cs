5c78e979ff37282baceaea6faa8dd089c7a16ee9
Chris Kaczor <chris@kaczorzoo.net>
2014-11-04 19:24:52 -0500

    Add async task for starting a process

---
+++ IO/ProcessAsync.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Common.IO
+{
+    public class ProcessAsync
+    {
+        public static Task RunProcessAsync(string fileName, string arguments)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = fileName,
+                    Arguments = arguments
+                },
+                EnableRaisingEvents = true
+            };
+
+            process.Exited += (sender, args) =>
+            {
+                taskCompletionSource.SetResult(true);
+                process.Dispose();
+            };
+
+            process.Start();
+
+            return taskCompletionSource.Task;
+        }
+    }
+}
