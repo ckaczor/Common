b327c474f3dd38773e817e9f2368a916933c534f
Chris Kaczor <chris@kaczorzoo.net>
2016-03-29 20:04:27 -0400

    Add GetAbsoluteUrlString

---
+++ Internet/UrlHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Common.Internet
+{
+    public static class UrlHelper
+    {
+        public static string GetAbsoluteUrlString(string baseUrl, string url)
+        {
+            var uri = new Uri(url, UriKind.RelativeOrAbsolute);
+            if (!uri.IsAbsoluteUri)
+                uri = new Uri(new Uri(baseUrl), uri);
+            return uri.ToString();
+        }
+    }
+}
